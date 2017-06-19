094654de272c5c7662aa51590e4c833e8b9ba32a
EsdrasVP <esdras015@gmail.com>
2017-06-19 11:05:54 -0300

    Changing param

--- core/plugins/identity/openstack/openstack_token_plugin.py
+++ core/plugins/identity/openstack/openstack_token_plugin.py
@@ -43,7 +43,7 @@ class OpenstackV3TokenGenerator(token_generator):
     @staticmethod
     def do_post_request(current_token_endpoint, json_payload):
         header = {ApplicationConstants.CONTENT_TYPE: ApplicationConstants.JSON_CONTENT_TYPE}
-        return requests.post(current_token_endpoint, data=json_payload, header=header)
+        return requests.post(current_token_endpoint, data=json_payload, headers=header)
 
     @staticmethod
     def get_token_from_response(response):
